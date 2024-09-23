a5bd552373ff2f53524d38421269f1da112e77e0
Toufik Badkar <toufikbadkar80@gmail.com>
2024-09-23 22:54:52 +0530

    Day15 Solution

---
+++ Day15/LongestSubStr.java
@@ -0,0 +1,24 @@
+package Day15;
+public class LongestSubStr {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> st = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+        
+        for(int i=0;i<s.length();i++){
+            if(!st.contains(s.charAt(i))){
+                maxLength = Math.max(maxLength,i-left+1);
+                st.add(s.charAt(i));
+            }
+            else{
+                while(st.contains(s.charAt(i))){
+                    st.remove(s.charAt(left));
+                    left++;
+                }
+                st.add(s.charAt(i));
+            }
+        }
+        
+        return maxLength;
+    }
+}
